84d3ba598d146238dc6a5b4ea65ce01f9eed36b1
herve <herve.mbilo@gmail.com>
2016-07-26 20:26:21 +0200

    Mycodejavascript version two

--- codes/symetricDifference.js
+++ codes/symetricDifference.js
@@ -11,17 +11,28 @@ function sym(args)
 	var output = []; // contain the output.
   var flattenArray = []; //all arrays from the seconds array will be flatteren...
 	var temp = arguments[0]; // contain the first array in the argumment.
-  var uniqueValueOutput = [];
+  var uniqueValueOutput = []; // to display.
+  var uniqueArray = [];
+  var args = [].slice.call(arguments);
 
+  console.log('array numéro : ' + (1) + ' => [' + temp + ']');
+  temp = temp.filter(function(item, pos) { // Remove all duplicate values from first array.
+    return temp.indexOf(item) == pos;
+  });
 	//-----------------------------------------------------------------// 1 step: We get all the elements from the second array.
     //console.log('array numéro : 1' + ' => [' + temp + ']');
-    for(var i = 1; i < arguments.length; i++){ // we loop from the second argument's array.
-    //console.log('array numéro : ' + (i + 1) + ' => [' + arguments[i] + ']');
-    	for(var j = 0; j < arguments[i].length; j++){ //We loop on each element of the array.
-        flattenArray.push(arguments[i][j]);
+    for(var i = 1; i < args.length; i++){ // we loop from the second argument's array.
+      uniqueArray = args[i].filter(function(item, pos){
+        return args[i].indexOf(item) == pos;
+      })
+        console.log('array numéro : ' + (i + 1) + ' => [' + args[i] + ']');
+    	for(var j = 0; j < uniqueArray.length; j++){ //We loop on each element of the array.
+
+        //console.log(uniqueArray[j]);
+        flattenArray.push(uniqueArray[j]);
     	}
     }
-    //console.log('flatteren : ' + flattenArray);
+    console.log('flatteren : ' + temp +','+ flattenArray);
 
     //-----------------------------------------------------------------// 2 step:
     for(var i = 0; i < flattenArray.length; i++){ //We loop on each element of the array.
@@ -39,11 +50,10 @@ function sym(args)
 
     //--------------------------------------------------------------// 3 step: concat and display the result.
     output = output.concat(temp).sort();
-    console.log(output);
 
     uniqueValueOutput = output.filter(function(v,i,o){
       if(v!==o[i-1] && v!==o[i+1]){ // If the index is bigger than 0 and current value is not equal to the previous value.
-        console.log('index: ' + i + ' = value: ' + v + ' = prev: '+ o[i-1]);
+        //console.log('index: ' + i + ' = value: ' + v + ' = prev: '+ o[i-1]);
         return v; // than return the value.
       }
     }); // Your filter
@@ -59,10 +69,10 @@ function sym(args)
 
 
 var display = document.querySelector('.test');
-display.innerHTML = sym([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5]); // result must be 1, 4, 5
+display.innerHTML = sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3]);
 
 // [1, 2, 3], [5, 2, 1, 4]                  return [3, 4, 5]
 // [1, 2, 5], [2, 3, 5], [3, 4, 5]          return [1, 4, 5]
 // [1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5] return [1, 4, 5]
-// [1, 2, 5], [2, 3, 5]                     return [1, 4, 5]
-// [1, 2, 5], [2, 3, 5], [3, 4, 5]          return [1, 4, 5]
+// [3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3] return [2, 3, 4, 6, 7]
+// [3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1] return [1, 2, 4, 5, 6, 7, 8, 9]
